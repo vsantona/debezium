3ffa8e3a5bd03b5468d7d92110f7f65ddd1cd1d6
“vsantonastaso” <vincenzo.santonastaso@gmail.com>
2023-11-07 09:55:22 +0100

    DBZ-6878 move notification test from abstract class to concrete

--- debezium-embedded/src/test/java/io/debezium/pipeline/notification/AbstractNotificationsIT.java
+++ debezium-embedded/src/test/java/io/debezium/pipeline/notification/AbstractNotificationsIT.java
@@ -5,17 +5,15 @@
  */
 package io.debezium.pipeline.notification;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.lang.management.ManagementFactory;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import io.debezium.config.CommonConnectorConfig;
+import io.debezium.config.Configuration;
+import io.debezium.embedded.AbstractConnectorTest;
+import io.debezium.junit.logging.LogInterceptor;
+import io.debezium.pipeline.notification.channels.SinkNotificationChannel;
+import io.debezium.pipeline.notification.channels.jmx.JmxNotificationChannelMXBean;
+import org.apache.kafka.connect.source.SourceConnector;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
 import javax.management.AttributeNotFoundException;
 import javax.management.InstanceNotFoundException;
@@ -30,24 +28,15 @@ import javax.management.MalformedObjectNameException;
 import javax.management.NotificationListener;
 import javax.management.ObjectName;
 import javax.management.ReflectionException;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import org.apache.kafka.connect.data.Struct;
-import org.apache.kafka.connect.source.SourceConnector;
-import org.apache.kafka.connect.source.SourceRecord;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.data.Percentage;
-import org.awaitility.Awaitility;
-import org.junit.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.debezium.config.CommonConnectorConfig;
-import io.debezium.config.Configuration;
-import io.debezium.embedded.AbstractConnectorTest;
-import io.debezium.junit.logging.LogInterceptor;
-import io.debezium.pipeline.notification.channels.SinkNotificationChannel;
-import io.debezium.pipeline.notification.channels.jmx.JmxNotificationChannelMXBean;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public abstract class AbstractNotificationsIT<T extends SourceConnector> extends AbstractConnectorTest {
 
@@ -77,42 +66,6 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
     protected abstract String snapshotStatusResult();
 
     @Test
-    public void notificationCorrectlySentOnItsTopic() throws InterruptedException {
-        // Testing.Print.enable();
-
-        startConnector(config -> config
-                .with(SinkNotificationChannel.NOTIFICATION_TOPIC, "io.debezium.notification")
-                .with(CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS, "sink"));
-
-        assertConnectorIsRunning();
-
-        waitForSnapshotToBeCompleted(connector(), server(), task(), database());
-
-        List<SourceRecord> notifications = new ArrayList<>();
-        Awaitility.await().atMost(60, TimeUnit.SECONDS).until(() -> {
-
-            consumeAvailableRecords(r -> {
-                if (r.topic().equals("io.debezium.notification")) {
-                    notifications.add(r);
-                }
-            });
-            return notifications.size() == 2;
-        });
-
-        assertThat(notifications).hasSize(2);
-        SourceRecord sourceRecord = notifications.get(0);
-        Assertions.assertThat(sourceRecord.topic()).isEqualTo("io.debezium.notification");
-        Assertions.assertThat(((Struct) sourceRecord.value()).getString("aggregate_type")).isEqualTo("Initial Snapshot");
-        Assertions.assertThat(((Struct) sourceRecord.value()).getString("type")).isEqualTo("STARTED");
-        Assertions.assertThat(((Struct) sourceRecord.value()).getInt64("timestamp")).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
-        sourceRecord = notifications.get(1);
-        Assertions.assertThat(sourceRecord.topic()).isEqualTo("io.debezium.notification");
-        Assertions.assertThat(((Struct) sourceRecord.value()).getString("aggregate_type")).isEqualTo("Initial Snapshot");
-        Assertions.assertThat(((Struct) sourceRecord.value()).getString("type")).isEqualTo(snapshotStatusResult());
-        Assertions.assertThat(((Struct) sourceRecord.value()).getInt64("timestamp")).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
-    }
-
-    @Test
     public void notificationNotSentIfNoChannelIsConfigured() {
         // Testing.Print.enable();
 
@@ -138,85 +91,7 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
                 .isTrue();
     }
 
-    @Test
-    public void notificationCorrectlySentOnJmx()
-            throws ReflectionException, MalformedObjectNameException, InstanceNotFoundException, IntrospectionException, AttributeNotFoundException,
-            MBeanException, InterruptedException {
-
-        // Testing.Print.enable();
-
-        startConnector(config -> config
-                .with(CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS, "jmx"));
-
-        assertConnectorIsRunning();
-
-        waitForSnapshotToBeCompleted(connector(), server(), task(), database());
-
-        Awaitility.await().atMost(30, TimeUnit.SECONDS)
-                .pollDelay(1, TimeUnit.SECONDS)
-                .pollInterval(1, TimeUnit.SECONDS)
-                .until(() -> !readNotificationFromJmx().isEmpty());
-
-        List<Notification> notifications = readNotificationFromJmx();
-
-        assertThat(notifications).hasSize(2);
-        assertThat(notifications.get(0))
-                .hasFieldOrPropertyWithValue("aggregateType", "Initial Snapshot")
-                .hasFieldOrPropertyWithValue("type", "STARTED")
-                .hasFieldOrProperty("timestamp");
-        assertThat(notifications.get(1))
-                .hasFieldOrPropertyWithValue("aggregateType", "Initial Snapshot")
-                .hasFieldOrPropertyWithValue("type", snapshotStatusResult())
-                .hasFieldOrProperty("timestamp");
-
-        resetNotifications();
-
-        notifications = readNotificationFromJmx();
-        assertThat(notifications).hasSize(0);
-    }
-
-    @Test
-    public void emittingDebeziumNotificationWillGenerateAJmxNotification()
-            throws ReflectionException, MalformedObjectNameException, InstanceNotFoundException, IntrospectionException, AttributeNotFoundException,
-            MBeanException, InterruptedException, JsonProcessingException {
-
-        // Testing.Print.enable();
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        startConnector(config -> config
-                .with(CommonConnectorConfig.SNAPSHOT_DELAY_MS, 2000)
-                .with(CommonConnectorConfig.NOTIFICATION_ENABLED_CHANNELS, "jmx"));
-
-        List<javax.management.Notification> jmxNotifications = registerJmxNotificationListener();
-
-        assertConnectorIsRunning();
-
-        waitForSnapshotToBeCompleted(connector(), server(), task(), database());
-
-        MBeanNotificationInfo[] notifications = readJmxNotifications();
-
-        assertThat(notifications).allSatisfy(mBeanNotificationInfo -> assertThat(mBeanNotificationInfo.getName()).isEqualTo(Notification.class.getName()));
-
-        assertThat(jmxNotifications).hasSize(2);
-        assertThat(jmxNotifications.get(0)).hasFieldOrPropertyWithValue("message", "Initial Snapshot generated a notification");
-        Notification notification = mapper.readValue(jmxNotifications.get(0).getUserData().toString(), Notification.class);
-        assertThat(notification)
-                .hasFieldOrPropertyWithValue("aggregateType", "Initial Snapshot")
-                .hasFieldOrPropertyWithValue("type", "STARTED")
-                .hasFieldOrPropertyWithValue("additionalData", Map.of("connector_name", server()));
-        assertThat(notification.getTimestamp()).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
-
-        assertThat(jmxNotifications.get(1)).hasFieldOrPropertyWithValue("message", "Initial Snapshot generated a notification");
-        notification = mapper.readValue(jmxNotifications.get(1).getUserData().toString(), Notification.class);
-        assertThat(notification)
-                .hasFieldOrPropertyWithValue("aggregateType", "Initial Snapshot")
-                .hasFieldOrPropertyWithValue("type", "COMPLETED")
-                .hasFieldOrPropertyWithValue("additionalData", Map.of("connector_name", server()));
-        assertThat(notification.getTimestamp()).isCloseTo(Instant.now().toEpochMilli(), Percentage.withPercentage(1));
-    }
-
-    private List<Notification> readNotificationFromJmx()
+    protected List<Notification> readNotificationFromJmx()
             throws MalformedObjectNameException, ReflectionException, InstanceNotFoundException, IntrospectionException, AttributeNotFoundException, MBeanException {
 
         ObjectName notificationBean = getObjectName();
@@ -235,7 +110,7 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
         return proxy.getNotifications();
     }
 
-    private MBeanNotificationInfo[] readJmxNotifications()
+    protected MBeanNotificationInfo[] readJmxNotifications()
             throws MalformedObjectNameException, ReflectionException, InstanceNotFoundException, IntrospectionException, AttributeNotFoundException, MBeanException {
 
         ObjectName notificationBean = getObjectName();
@@ -251,7 +126,7 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
         return new ObjectName(String.format("debezium.%s:type=management,context=notifications,server=%s", connector(), server()));
     }
 
-    private List<javax.management.Notification> registerJmxNotificationListener()
+    protected List<javax.management.Notification> registerJmxNotificationListener()
             throws MalformedObjectNameException, InstanceNotFoundException {
 
         ObjectName notificationBean = getObjectName();
@@ -263,7 +138,7 @@ public abstract class AbstractNotificationsIT<T extends SourceConnector> extends
         return receivedNotifications;
     }
 
-    private void resetNotifications()
+    protected void resetNotifications()
             throws MalformedObjectNameException, ReflectionException, InstanceNotFoundException, MBeanException {
 
         ObjectName notificationBean = getObjectName();
