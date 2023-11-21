93b9cabb699a84ef64fd3cf391b3f3f669353475
“vsantonastaso” <vincenzo.santonastaso@gmail.com>
2023-11-21 10:21:53 +0100

    DBZ-6878 add table specific notification in initial snapshot

--- debezium-connector-sqlserver/src/test/java/io/debezium/connector/sqlserver/NotificationsIT.java
+++ debezium-connector-sqlserver/src/test/java/io/debezium/connector/sqlserver/NotificationsIT.java
@@ -53,6 +53,10 @@ public class NotificationsIT extends AbstractNotificationsIT<SqlServerConnector>
         TestHelper.dropTestDatabase();
     }
 
+    protected List<String> collections() {
+        return List.of("tablea", "tableb");
+    }
+
     @Override
     protected Class<SqlServerConnector> connectorClass() {
         return SqlServerConnector.class;
@@ -61,7 +65,7 @@ public class NotificationsIT extends AbstractNotificationsIT<SqlServerConnector>
     @Override
     protected Configuration.Builder config() {
         return TestHelper.defaultConfig()
-                .with(SqlServerConnectorConfig.SNAPSHOT_MODE, SqlServerConnectorConfig.SnapshotMode.SCHEMA_ONLY);
+                .with(SqlServerConnectorConfig.SNAPSHOT_MODE, SqlServerConnectorConfig.SnapshotMode.INITIAL);
     }
 
     @Override
